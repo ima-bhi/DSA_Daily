0537b7c942747943899b8efd9e6f7388aa74341f
Abhishek Kumar <abhishekkumar.as8@krishnacollege.ac.in>
2024-06-04 17:07:29 +0000

    learning binary search

---
+++ Array/binary_search.cpp
@@ -0,0 +1,18 @@
+
+//fxn
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid; // Target found
+            } else if (nums[mid] < target) {
+                left = mid + 1; // Discard left half
+            } else {
+                right = mid - 1; // Discard right half
+            }
+        }
+        return -1; // Target not found
+    }
